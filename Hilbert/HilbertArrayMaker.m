74a4a913541067b2cf5d1925c94a6ca8e6a52b0c
amirhosein safari <amirsfr53@gmail.com>
2018-09-07 17:09:00 +0200

    Update HilbertArrayMaker.m

--- Hilbert/HilbertArrayMaker.m
+++ Hilbert/HilbertArrayMaker.m
@@ -13,6 +13,10 @@ function [FinalArray] = HilbertArrayMaker(NumberOfCubesInRow, NumberOfCubesInCol
     %
     % loop over the number of rows and columns in our final array
     
+    numOfVoxelsinRowAndCulomn = 60; % 60 is the num of voxels in x and y and z axis
+    numOfVoxelsinBorder = 10; % 10 is the num of voxels of thickness of border.
+    thicknessOfCube = numOfVoxelsinRowAndCulomn + numOfVoxelsinBorder;
+    
     for i1 = 1 : NumberOfCubesInRow
         for i2 = 1 : NumberOfCubesInColumn
             disp(i2);
@@ -48,9 +52,9 @@ function [FinalArray] = HilbertArrayMaker(NumberOfCubesInRow, NumberOfCubesInCol
             arr = arr.arr;
 
             % Add randomeness to our cubes! we reduces some lines randomely
-            for ii1 = 1 : min(size(arr,1) , 60)
-                for ii2 = 1 : min(size(arr,2) , 60)
-                    for ii3 = 1 : min(size(arr,3) , 60)
+            for ii1 = 1 : min(size(arr,1) , numOfVoxelsinRowAndCulomn)
+                for ii2 = 1 : min(size(arr,2) , numOfVoxelsinRowAndCulomn)
+                    for ii3 = 1 : min(size(arr,3) , numOfVoxelsinRowAndCulomn)
                         randf = randi ([0 , 10]) + 1;
                         if arr(ii1,ii2,ii3) == 1 && randf < randOfrand
                            arr(ii1,ii2,ii3) = 0;  
@@ -67,32 +71,32 @@ function [FinalArray] = HilbertArrayMaker(NumberOfCubesInRow, NumberOfCubesInCol
             s = size (arr);
 
             % put base zBase layer, for example here we have 50 layer of base
-            for i = 1 : 60
-                for j = 1 : 60
+            for i = 1 : numOfVoxelsinRowAndCulomn
+                for j = 1 : numOfVoxelsinRowAndCulomn
                     for z = 1 : zBase
                        FinalArray(i,j,z) = 0; 
                     end
                 end
             end
 
-            % if the dimension of loaded cube is less than 60*60*60 then we
+            % if the dimension of loaded cube is less than numOfVoxelsinRowAndCulomn*numOfVoxelsinRowAndCulomn*numOfVoxelsinRowAndCulomn then we
             % magnify the array otherwise we'll compress it!  
-            if s(1) < 60
-                tmp = ones(round(60 / s(1)));
-                tmp2 = ones(round(60 / s(2)));
-                t2 = round(60 / s(2));
-                if (round(60 / s(1)) == 1)
+            if s(1) < numOfVoxelsinRowAndCulomn
+                tmp = ones(round(numOfVoxelsinRowAndCulomn / s(1)));
+                tmp2 = ones(round(numOfVoxelsinRowAndCulomn / s(2)));
+                t2 = round(numOfVoxelsinRowAndCulomn / s(2));
+                if (round(numOfVoxelsinRowAndCulomn / s(1)) == 1)
                     tmp = ones(2);
                 end
                 for z = 1 : s(3)
                     a2 = kron(arr(:,:,z),tmp);
                     sizeofa2 = size(a2);
-                    pada2 = zeros(60);
+                    pada2 = zeros(numOfVoxelsinRowAndCulomn);
                     pada2 (1:sizeofa2(1),1:sizeofa2(2)) = a2;
                     a2 = pada2;
                     if randOfLevel == 1 
-                        for ii1 = 1 : 60
-                            for ii2 = 1 : 60
+                        for ii1 = 1 : numOfVoxelsinRowAndCulomn
+                            for ii2 = 1 : numOfVoxelsinRowAndCulomn
                                 randd = randi([0 3]) + 1;
                                 if a2(ii1,ii2) == 1 && randd == 1
                                     a2(ii1,ii2) = 0;
@@ -102,44 +106,44 @@ function [FinalArray] = HilbertArrayMaker(NumberOfCubesInRow, NumberOfCubesInCol
                     end
 
                     for tt = 1 : t2
-                        if ((z - 1) * t2 + tt <= 60)
-                            FinalArray(((i1 - 1) * 70) + 1:((i1 - 1) * 70) + 60, ((i2 - 1) * 70) + 1 :((i2 - 1) * 70) + 60,(z - 1) * t2 + tt + zBase) = a2(1:60,1:60);
+                        if ((z - 1) * t2 + tt <= numOfVoxelsinRowAndCulomn)
+                            FinalArray(((i1 - 1) * thicknessOfCube) + 1:((i1 - 1) * thicknessOfCube) + numOfVoxelsinRowAndCulomn, ((i2 - 1) * thicknessOfCube) + 1 :((i2 - 1) * thicknessOfCube) + numOfVoxelsinRowAndCulomn,(z - 1) * t2 + tt + zBase) = a2(1:numOfVoxelsinRowAndCulomn,1:numOfVoxelsinRowAndCulomn);
                         end
                     end
                 end
             else
-                tmp = round(s(1) / 60);
-                tmp2 = round(s(2) / 60);
-                t2 = round(60 / s(2));
-                for z = 1 : 60
-                    if (z <= 60)
-                        FinalArray(((i1 - 1) * 70) + 1:((i1 - 1) * 70) + 60, ((i2 - 1) * 70) + 1 :((i2 - 1) * 70) + 60,z + zBase) = arr(1:60, 1:60,z);
+                tmp = round(s(1) / numOfVoxelsinRowAndCulomn);
+                tmp2 = round(s(2) / numOfVoxelsinRowAndCulomn);
+                t2 = round(numOfVoxelsinRowAndCulomn / s(2));
+                for z = 1 : numOfVoxelsinRowAndCulomn
+                    if (z <= numOfVoxelsinRowAndCulomn)
+                        FinalArray(((i1 - 1) * thicknessOfCube) + 1:((i1 - 1) * thicknessOfCube) + numOfVoxelsinRowAndCulomn, ((i2 - 1) * thicknessOfCube) + 1 :((i2 - 1) * thicknessOfCube) + numOfVoxelsinRowAndCulomn,z + zBase) = arr(1:numOfVoxelsinRowAndCulomn, 1:numOfVoxelsinRowAndCulomn,z);
                     end
                 end
             end
 
 
             % put right and button border
-            for i = 1 : 10
-                for j = 1 : 70
-                    for z = 1 : 60 + zBase
-                        FinalArray(i + (i1 - 1) * 70 + 60 ,j + (i2 - 1) * 70,z) = 1;
+            for i = 1 : numOfVoxelsinBorder
+                for j = 1 : thicknessOfCube
+                    for z = 1 : numOfVoxelsinRowAndCulomn + zBase
+                        FinalArray(i + (i1 - 1) * thicknessOfCube + numOfVoxelsinRowAndCulomn ,j + (i2 - 1) * thicknessOfCube,z) = 1;
                     end
                 end
             end
-            for i = 1 : 70
-                for j = 1 : 10
-                    for z = 1 : 60 + zBase
-                        FinalArray(i + (i1 - 1) * 70 ,j + (i2 - 1) * 70 + 60,z) = 1;
+            for i = 1 : thicknessOfCube
+                for j = 1 : numOfVoxelsinBorder
+                    for z = 1 : numOfVoxelsinRowAndCulomn + zBase
+                        FinalArray(i + (i1 - 1) * thicknessOfCube ,j + (i2 - 1) * thicknessOfCube + numOfVoxelsinRowAndCulomn,z) = 1;
                     end
                 end
             end
-            % each cube with border is 70 * 70 px
+            % each cube with border is thicknessOfCube * thicknessOfCube px
         end
     end
 
     % save the layers as fig
-    for ii = 50 : 110
+    for ii = zBase : zBase + numOfVoxelsinRowAndCulomn
        imagesc(FinalArray(:,:,ii));
        savefig(strcat('H',int2str(ii),'.fig'));
     end
