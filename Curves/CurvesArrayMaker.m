cb8d2540b71c7684c9beed5a7d60265e04a57fc5
amirhosein safari <amirsfr53@gmail.com>
2018-09-07 17:09:52 +0200

    Update CurvesArrayMaker.m

--- Curves/CurvesArrayMaker.m
+++ Curves/CurvesArrayMaker.m
@@ -13,16 +13,20 @@ function [FinalArray] = CurvesArrayMaker(NumberOfCubesInRow, NumberOfCubesInColu
     %           zBase: a number which reperesent number of layers of the base
     %
     % loop over the number of rows and columns in our final array
-    numOfSamples = 70 - 1;   % 70 is number of curves file that we have so it's a constant!
+    
+    numOfSamples = 70 - 1;  % 70 is number of curves file that we have so it's a constant!
+    numOfVoxelsinRowAndCulomn = 60; % 60 is the num of voxels in x and y axis
+    numOfVoxelsinBorder = 10; % 10 is the num of voxels of thickness of border.
+    thicknessOfCube = numOfVoxelsinRowAndCulomn + numOfVoxelsinBorder;
     for i1 = 1 : NumberOfCubesInRow
-        for i2 = 1 : NumberOfCubesInColumn
+        for i2 = 1 : NumberOfCubesInColumn  
+
             disp (i1);
             disp (i2);
 
             % put base zBase layer, for example here we have 50 layer of base
-            % 60 is the number of voxels in x and y axis!
-            for i = 1 : 60
-                for j = 1 : 60
+            for i = 1 : numOfVoxelsinRowAndCulomn
+                for j = 1 : numOfVoxelsinRowAndCulomn
                     for z = 1 : zBase
                        FinalArray(i,j,z) = 0; 
                     end
@@ -35,35 +39,35 @@ function [FinalArray] = CurvesArrayMaker(NumberOfCubesInRow, NumberOfCubesInColu
                  arr = load(strcat(num2str(num),'.mat'));
                  arr = arr.res;
 
-                 % if the dimension of loaded cube is less than 60*60*60 then we
+                 % if the dimension of loaded cube is less than numOfVoxelsinRowAndCulomn*numOfVoxelsinRowAndCulomn*numOfVoxelsinRowAndCulomn then we
                  % magnify the array otherwise we'll cut it!  
-                 if size(arr , 1) >= 60 || size(arr , 2) >= 60
-                     pada2 = zeros(60);
+                 if size(arr , 1) >= numOfVoxelsinRowAndCulomn || size(arr , 2) >= numOfVoxelsinRowAndCulomn
+                     pada2 = zeros(numOfVoxelsinRowAndCulomn);
                      pada2 (1:size(arr , 1),1:size(arr , 2)) = arr;
-                     arr = pada2 (1:60,1:60);
+                     arr = pada2 (1:numOfVoxelsinRowAndCulomn,1:numOfVoxelsinRowAndCulomn);
                  else 
-                     tmp = ones(round(60 / size(arr , 1)));
-                     if (round(60 / size(arr , 1)) == 1)
+                     tmp = ones(round(numOfVoxelsinRowAndCulomn / size(arr , 1)));
+                     if (round(numOfVoxelsinRowAndCulomn / size(arr , 1)) == 1)
                         tmp = ones(2);
                      end
 
                      a2 = kron(arr,tmp);
-                     if size(a2,1) > 60
-                         a2 = a2(1:60,:);
+                     if size(a2,1) > numOfVoxelsinRowAndCulomn
+                         a2 = a2(1:numOfVoxelsinRowAndCulomn,:);
                      end
-                     if size(a2,2) > 60
-                         a2 = a2(:,1:60);
+                     if size(a2,2) > numOfVoxelsinRowAndCulomn
+                         a2 = a2(:,1:numOfVoxelsinRowAndCulomn);
                      end
                      sizeofa2 = size(a2);
 
-                     pada2 = zeros(60);
+                     pada2 = zeros(numOfVoxelsinRowAndCulomn);
                      pada2 (1:sizeofa2(1),1:sizeofa2(2)) = a2;
                      arr = pada2;
                  end
 
                  % Add randomeness to our cubes! we reduces some points randomely
-                 for i3 = 1 : 60
-                     for i4 = 1 : 60
+                 for i3 = 1 : numOfVoxelsinRowAndCulomn
+                     for i4 = 1 : numOfVoxelsinRowAndCulomn
                         rand = randi([0 3]) + 1;
                         if rand == 1 && arr (i3,i4) == 1
                             arr(i3,i4) = 0;
@@ -72,34 +76,34 @@ function [FinalArray] = CurvesArrayMaker(NumberOfCubesInRow, NumberOfCubesInColu
                  end
 
                  % add the cube to the a array
-                 FinalArray((i1 - 1) * 70 + 1:(i1 - 1) * 70 + 60, (i2 - 1) * 70 + 1:(i2 - 1) * 70 + 60, (i - 1) * 4 + 1 +zBase) = arr;
-                 FinalArray((i1 - 1) * 70 + 1:(i1 - 1) * 70 + 60, (i2 - 1) * 70 + 1:(i2 - 1) * 70 + 60, (i - 1) * 4 + 2 +zBase) = arr;
-                 FinalArray((i1 - 1) * 70 + 1:(i1 - 1) * 70 + 60, (i2 - 1) * 70 + 1:(i2 - 1) * 70 + 60, (i - 1) * 4 + 3 +zBase) = arr;
-                 FinalArray((i1 - 1) * 70 + 1:(i1 - 1) * 70 + 60, (i2 - 1) * 70 + 1:(i2 - 1) * 70 + 60, (i - 1) * 4 + 4 +zBase) = arr;
+                 FinalArray((i1 - 1) * thicknessOfCube + 1:(i1 - 1) * thicknessOfCube + numOfVoxelsinRowAndCulomn, (i2 - 1) * thicknessOfCube + 1:(i2 - 1) * thicknessOfCube + numOfVoxelsinRowAndCulomn, (i - 1) * 4 + 1 +zBase) = arr;
+                 FinalArray((i1 - 1) * thicknessOfCube + 1:(i1 - 1) * thicknessOfCube + numOfVoxelsinRowAndCulomn, (i2 - 1) * thicknessOfCube + 1:(i2 - 1) * thicknessOfCube + numOfVoxelsinRowAndCulomn, (i - 1) * 4 + 2 +zBase) = arr;
+                 FinalArray((i1 - 1) * thicknessOfCube + 1:(i1 - 1) * thicknessOfCube + numOfVoxelsinRowAndCulomn, (i2 - 1) * thicknessOfCube + 1:(i2 - 1) * thicknessOfCube + numOfVoxelsinRowAndCulomn, (i - 1) * 4 + 3 +zBase) = arr;
+                 FinalArray((i1 - 1) * thicknessOfCube + 1:(i1 - 1) * thicknessOfCube + numOfVoxelsinRowAndCulomn, (i2 - 1) * thicknessOfCube + 1:(i2 - 1) * thicknessOfCube + numOfVoxelsinRowAndCulomn, (i - 1) * 4 + 4 +zBase) = arr;
             end
 
             % put right and button border
             for i = 1 : 10
-                for j = 1 : 70
-                    for z = 1 : 60 + zBase
-                        FinalArray(i + (i1 - 1) * 70 + 60 ,j + (i2 - 1) * 70,z) = 1;
+                for j = 1 : thicknessOfCube
+                    for z = 1 : numOfVoxelsinRowAndCulomn + zBase
+                        FinalArray(i + (i1 - 1) * thicknessOfCube + numOfVoxelsinRowAndCulomn ,j + (i2 - 1) * thicknessOfCube,z) = 1;
                     end
                 end
             end
-            for i = 1 : 70
+            for i = 1 : thicknessOfCube
                 for j = 1 : 10
-                    for z = 1 : 60 + zBase
-                        FinalArray(i + (i1 - 1) * 70 ,j + (i2 - 1) * 70 + 60,z) = 1;
+                    for z = 1 : numOfVoxelsinRowAndCulomn + zBase
+                        FinalArray(i + (i1 - 1) * thicknessOfCube ,j + (i2 - 1) * thicknessOfCube + numOfVoxelsinRowAndCulomn,z) = 1;
                     end
                 end
             end
-            % each cube with border is 70 * 70 px
+            % each cube with border is thicknessOfCube * thicknessOfCube px
         end
     end
 
     % save the layers as fig
 
-    for ii = 1 : 110 % this is the number of total z layers
+    for ii = 1 : numOfVoxelsinRowAndCulomn + zBase
        imagesc(FinalArray(:,:,ii));
        savefig(strcat(int2str(ii),'.fig'));
     end
